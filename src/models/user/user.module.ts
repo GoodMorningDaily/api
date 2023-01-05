965bde6c5ac0810bed0464e0cd968c7f728fb25c
Matheus Ribeiro <matheus.junio159@gmail.com>
2023-01-05 08:38:49 -0300

    adjustment(user): move user folder to models folder.

--- src/models/user/user.module.ts
+++ src/models/user/user.module.ts
@@ -1,7 +1,7 @@
-import { PrismaService } from './../prisma.service';
-import { Module } from '@nestjs/common';
-import { UserService } from './service/user.service';
+import { PrismaService } from './../../prisma.service';
 import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
+import { Module } from '@nestjs/common';
 
 @Module({
   providers: [UserService, PrismaService],
